205f7101e96c7da10758d5b74e194d4f070ac2c5
SooKee <oasookee@gmail.com>
2015-03-11 15:46:37 +0000

    fix scripts

--- configure-debug.sh
+++ configure-debug.sh
@@ -7,9 +7,9 @@ top_dir=$(pwd)
 PREFIX=${PREFIX:-$HOME/dev}
 LIBDIR=$PREFIX/lib
 
-export PKG_CONFIG_PATH=$LIBDIR/pkgconfig
+export PKG_CONFIG_PATH="$LIBDIR/pkgconfig"
 
-export CXXFLAGS=-g3 -O0 -D DEBUG
+export CXXFLAGS="-g3 -O0 -D DEBUG"
 
 rm -fr $top_dir/build-debug
 mkdir -p $top_dir/build-debug
--- configure-release.sh
+++ configure-release.sh
@@ -7,9 +7,9 @@ top_dir=$(pwd)
 PREFIX=${PREFIX:-$HOME}
 LIBDIR=$PREFIX/lib
 
-export PKG_CONFIG_PATH=$LIBDIR/pkgconfig
+export PKG_CONFIG_PATH="$LIBDIR/pkgconfig"
 
-export CXXFLAGS=-g0 -O3 -D NDEBUG
+export CXXFLAGS="-g0 -O3 -D NDEBUG"
 
 rm -fr $top_dir/build-release
 mkdir -p $top_dir/build-release
