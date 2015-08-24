205846214b662137c1b2ee75509b6454679e31d4
SooKee <oasookee@gmail.com>
2015-08-24 04:10:46 +0100

    fix header

--- src/i18n.cpp
+++ src/i18n.cpp
@@ -30,7 +30,9 @@ http://www.gnu.org/licenses/gpl-2.0.html
 #include <iconv.h>
 #include <cstring>
 #include <memory>
-#include <wchar.h>
+#include <cwchar>
+#include <uchar.h>
+
 
 //#include <unicode/utypes.h>
 //#include <unicode/ucnv.h>
